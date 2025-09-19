61f0a31e0a8f93ca7f080e6c92ea7aec18d14814
SamuelLobl <samuel.lobl@my.pbsc.edu>
2025-09-19 14:12:28 -0400

    Update Csharp Module 3 Discussion.cs

--- Csharp Module 3 Discussion.cs
+++ Csharp Module 3 Discussion.cs
@@ -1,9 +1,20 @@
 using system;
 
-public class Wine
+public class Alcohol
 {
-  public decimal Price;
-  public int Year;
-  public Wine (decimal price) => Price = price;
-  public Wine (decimal price, int year) : this (price) => Year = year;
+  public decimal Cost;
+  public int Age;
+  public Alcohol (decimal cost) => Cost = cost;
+  public Alcohol (decimal cost, int age) : this (cost) => Age = age;
 }
+
+public class Wine : Alcohol
+{
+    public bool Grapeflavored;
+
+    public Wine(decimal cost, int age) : base(cost, age)
+    {
+        this.Grapeflavored = true;
+    }
+}
+
